35f138b45defeed2f7e0ca6245dd5395831fb847
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2022-04-05 17:31:21 +0300

    JWT

---
+++ Core/Extensions/ClaimExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public static class ClaimExtensions
+    {
+        public static void AddName(this ICollection<Claim> claims, string name)
+        {
+            claims.Add(new Claim("name", name));
+        }
+
+        public static void AddId(this ICollection<Claim> claims, int id)
+        {
+            claims.Add(new Claim("id", id.ToString()));
+        }
+        public static void AddRoles(this ICollection<Claim> claims, string[] roles)
+        {
+            roles.ToList().ForEach(role => claims.Add(new Claim("roles", role)));
+        }
+    }
+}
